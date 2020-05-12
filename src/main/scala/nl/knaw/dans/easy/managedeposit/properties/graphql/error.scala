c1cb9bb17eb18e0907027751cf682d6355a855f4
Richard van Heest <richard.van.heest@dans.knaw.nl>
2020-05-12 10:33:13 +0200

    fix compile errors

--- src/main/scala/nl/knaw/dans/easy/managedeposit/properties/graphql/error.scala
+++ src/main/scala/nl/knaw/dans/easy/managedeposit/properties/graphql/error.scala
@@ -16,18 +16,8 @@
 package nl.knaw.dans.easy.managedeposit.properties.graphql
 
 object error {
-  abstract class GraphQLError(val msg: String) extends Exception(msg) {
-    def toSwordError: SwordError
-  }
-  case class UnexpectedResponseCode(code: Int, body: String) extends GraphQLError(s"GraphQL service returned code $code with message: $body") {
-    override def toSwordError: SwordError = new SwordError(500)
-  }
-  case class GraphQLQueryError(errors: List[String]) extends GraphQLError(s"Error in GraphQL query: ${ errors.mkString("[", ", ", "]") }") {
-    override def toSwordError: SwordError = new SwordError(500)
-  }
-  case object GraphQLServiceUnavailable extends GraphQLError("GraphQL service is unavailable") {
-    override def toSwordError: SwordError = new SwordError(503) {
-      override def getMessage: String = "503 Service temporarily unavailable"
-    }
-  }
+  abstract class GraphQLError(val msg: String) extends Exception(msg)
+  case class UnexpectedResponseCode(code: Int, body: String) extends GraphQLError(s"GraphQL service returned code $code with message: $body")
+  case class GraphQLQueryError(errors: List[String]) extends GraphQLError(s"Error in GraphQL query: ${ errors.mkString("[", ", ", "]") }")
+  case object GraphQLServiceUnavailable extends GraphQLError("GraphQL service is unavailable")
 }
