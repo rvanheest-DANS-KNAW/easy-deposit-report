850254737fc0c6cbd3d6d895502b6bd1bf51b073
Richard van Heest <richard.van.heest@dans.knaw.nl>
2020-05-13 19:44:03 +0200

    Implement ListDepositsToBeCleaned GraphQL query

--- src/main/scala/nl/knaw/dans/easy/managedeposit/properties/ServiceDepositPropertiesRepository.scala
+++ src/main/scala/nl/knaw/dans/easy/managedeposit/properties/ServiceDepositPropertiesRepository.scala
@@ -1,16 +1,23 @@
 package nl.knaw.dans.easy.managedeposit.properties
 
-import nl.knaw.dans.easy.managedeposit
+import better.files.File
 import nl.knaw.dans.easy.managedeposit.State.State
-import nl.knaw.dans.easy.managedeposit.properties.graphql.GraphQLClient
 import nl.knaw.dans.easy.managedeposit._
-import nl.knaw.dans.easy.managedeposit.properties.ServiceDepositPropertiesRepository.FindByDatasetId
-import org.json4s.Formats
+import nl.knaw.dans.easy.managedeposit.properties.ServiceDepositPropertiesRepository.{ FindByDatasetId, ListDepositsToBeCleaned }
+import nl.knaw.dans.easy.managedeposit.properties.graphql.GraphQLClient
+import nl.knaw.dans.easy.managedeposit.properties.graphql.direction.Forwards
+import org.joda.time.{ DateTime, DateTimeZone }
+import org.json4s.JsonAST.{ JInt, JString }
 import org.json4s.JsonDSL._
+import org.json4s.{ Formats, JValue }
 
 import scala.util.{ Failure, Success, Try }
 
-class ServiceDepositPropertiesRepository(client: GraphQLClient)(implicit formats: Formats) extends DepositPropertiesRepository {
+class ServiceDepositPropertiesRepository(client: GraphQLClient,
+                                         sword2DepositsDir: File,
+                                         ingestFlowInbox: File,
+                                         ingestFlowInboxArchived: Option[File],
+                                        )(implicit formats: Formats) extends DepositPropertiesRepository {
 
   override def load(depositId: DepositId): Try[DepositProperties] = Try {
     new ServiceDepositProperties(depositId, client)
@@ -18,23 +25,66 @@ class ServiceDepositPropertiesRepository(client: GraphQLClient)(implicit formats
 
   override def listReportData(depositor: Option[DepositorId], datamanager: Option[Datamanager], age: Option[Age]): Try[Stream[DepositInformation]] = ???
 
+  private def now: DateTime = DateTime.now(DateTimeZone.UTC)
+
   override def getCurationParametersByDatasetId(datasetId: DatasetId): Try[(DepositId, Option[State])] = {
     for {
       json <- client.doQuery(FindByDatasetId.query, FindByDatasetId.operationName, Map("datasetId" -> datasetId)).toTry
       identifier = json.extract[FindByDatasetId.Data].identifier
       result <- identifier
-          .map(identifier => {
-            val deposit = identifier.deposit
-            Success(deposit.depositId -> deposit.state.map(_.label))
-          })
-          .getOrElse(Failure(DatasetDoesNotExist(datasetId)))
+        .map(identifier => {
+          val deposit = identifier.deposit
+          Success(deposit.depositId -> deposit.state.map(_.label))
+        })
+        .getOrElse(Failure(DatasetDoesNotExist(datasetId)))
     } yield result
   }
 
   override def listDepositsToBeCleaned(filterOnDepositor: Option[DepositorId],
                                        filterOnAge: Age,
                                        filterOnState: State,
-                                      ): Try[Stream[DepositProperties with FileSystemDeposit]] = ???
+                                      ): Try[Stream[DepositProperties with FileSystemDeposit]] = {
+    implicit val convertJson: Any => JValue = {
+      case s: String => JString(s)
+      case i: Int => JInt(i)
+    }
+    val variables = Map(
+      "earlierThan" -> now.minusDays(filterOnAge).toString(dateTimeFormatter),
+      "state" -> filterOnState.toString,
+      "count" -> 100,
+    )
+    filterOnDepositor
+      .map(depositorId => {
+        client.doPaginatedQuery[ListDepositsToBeCleaned.DataWithDepositor](
+          query = ListDepositsToBeCleaned.queryWithDepositor,
+          operationName = ListDepositsToBeCleaned.operationName,
+          variables = variables + ("depositorId" -> depositorId),
+          direction = Forwards,
+        )(_.depositor.deposits.pageInfo)
+          .map(_.map(_.depositor.deposits))
+      })
+      .getOrElse {
+        client.doPaginatedQuery[ListDepositsToBeCleaned.DataWithoutDepositor](
+          query = ListDepositsToBeCleaned.queryWithoutDepositor,
+          operationName = ListDepositsToBeCleaned.operationName,
+          variables = variables,
+          direction = Forwards,
+        )(_.deposits.pageInfo)
+          .map(_.map(_.deposits))
+      }
+      .map(_.toStream.flatMap(_.edges.map(_.node.depositId).map(mkDepositProperties)))
+  }
+
+  private def mkDepositProperties(depositId: DepositId): DepositProperties with FileSystemDeposit = {
+    new ServiceDepositProperties(depositId, client) with FileSystemDeposit {
+      override protected val depositPath: Deposit = {
+        (sword2DepositsDir #:: ingestFlowInbox #:: ingestFlowInboxArchived.toStream)
+          .map(_ / depositId)
+          .find(_.exists)
+          .getOrElse { throw DepositDoesNotExist(depositId) }
+      }
+    }
+  }
 }
 
 object ServiceDepositPropertiesRepository {
@@ -53,6 +103,47 @@ object ServiceDepositPropertiesRepository {
         |      depositId
         |      state {
         |        label
+        |      }
+        |    }
+        |  }
+        |}""".stripMargin
+  }
+
+  object ListDepositsToBeCleaned {
+    case class DataWithDepositor(depositor: Depositor)
+    case class DataWithoutDepositor(deposits: Deposits)
+    case class Depositor(deposits: Deposits)
+    case class Deposits(pageInfo: Forwards, edges: Seq[Edge])
+    case class Edge(node: Node)
+    case class Node(depositId: DepositId)
+
+    val operationName = "ListDepositsToBeCleaned"
+    val queryWithDepositor: String =
+      """query ListDepositsToBeCleaned($depositorId: String!, $earlierThan: DateTime, $state: StateLabel!, $count: Int!, $after: String) {
+        |  depositor(id: $depositorId) {
+        |    deposits(earlierThan: $earlierThan, state: {label: $state, filter: LATEST}, first: $count, after: $after) {
+        |      pageInfo {
+        |        hasNextPage
+        |        endCursor
+        |      }
+        |      edges {
+        |        node {
+        |          depositId
+        |        }
+        |      }
+        |    }
+        |  }
+        |}""".stripMargin
+    val queryWithoutDepositor: String =
+      """query ListDepositsToBeCleaned($earlierThan: DateTime, $state: StateLabel!, $count: Int!, $after: String) {
+        |  deposits(earlierThan: $earlierThan, state: {label: $state, filter: LATEST}, first: $count, after: $after) {
+        |    pageInfo {
+        |        hasNextPage
+        |        endCursor
+        |    }
+        |    edges {
+        |      node {
+        |        depositId
         |      }
         |    }
         |  }
